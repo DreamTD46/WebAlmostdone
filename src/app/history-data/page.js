d7d4a037f5024d97ff54df993f377c1656adde17
DreamTD46 <ttiggerd@gmail.com>
2025-06-21 09:32:40 +0700

    add all file

---
+++ src/app/history-data/page.js
@@ -0,0 +1,17 @@
+// // src/app/history-data/page.js
+// 'use client'
+
+// import { Header, Footer } from '../../components/MonitoringInterface'
+// import HistoryData from '../../components/HistoryData'
+
+// export default function HistoryDataPage() {
+//     return (
+//         <div className="h-screen flex flex-col overflow-hidden bg-gray-50">
+//             <Header />
+//             <main className="flex-1 overflow-auto">
+//                 <HistoryData />
+//             </main>
+//             <Footer />
+//         </div>
+//     )
+// }
